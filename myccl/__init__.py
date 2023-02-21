ed395eb331933580ce6432192ca8385267d4b40d
Nick Koukoufilippas <nikfilippas@gmail.com>
2023-02-21 15:00:43 +0200

    patch for numpy alias deprecations

--- myccl/__init__.py
+++ myccl/__init__.py
@@ -1,4 +1,12 @@
 # flake8: noqa
+
+# Patch for deprecated alias in Numpy >= 1.20.0 (used in ISiTGR & FAST-PT).
+# Deprecation cycle starts in Numpy 1.20 and ends in Numpy 1.24.
+from packaging.version import parse
+import numpy
+numpy.int = int if parse(numpy.__version__) >= parse("1.20.0") else np.int
+del parse, numpy
+
 from .core import *
 from .parameters import *
 from .interpolate import *
